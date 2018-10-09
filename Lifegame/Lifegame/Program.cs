55dae14a3ed5dd670441e9c05f148ecad1d616ce
Tony <usbhub3.0@gmail.com>
2018-10-09 23:51:37 +0300

    init

---
+++ Lifegame/Lifegame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lifegame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
