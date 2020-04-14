2fd963df1001f838a1d2b28848237fe22f57db5b
Vacumsio <vacumsio@gmail.com>
2020-04-14 16:07:46 +0300

    So that, before life massives

--- Lifegame/Lifegame/LIfeGameEngine.cs
+++ Lifegame/Lifegame/LIfeGameEngine.cs
@@ -7,50 +7,55 @@ namespace Lifegame
         public static Field GetNextGeneration(Field currentField)
         {
             var resultField = new Field(currentField.Rows, currentField.Cols);
-            
-            currentField.Filed[0][0] = true;
-            Console.WriteLine(currentField.Filed[0][0]);
+
+            Console.WriteLine("\n Следующее поколение \n");
             int counter;
+            bool cur;
 
             for (int i = 0; i < currentField.Rows; i++)
             {
-
                 for (int j = 0; j < currentField.Cols; j++)
                 {
-
                     for (int g = 0; g < 7; g++)
                     {
-                        var cur = currentField.Filed[i][j];
+                        cur = currentField.Filed[i][j];
                         counter = 0;
 
                         if (i >= 1 && j >= 1)//1
                         {
                             if (currentField.Filed[i - 1][j - 1] == true) { counter++; }
                         }
+
                         if (i >= 1)//2
                         {
                             if (currentField.Filed[i - 1][j] == true) { counter++; }
                         }
-                        if (i >= 1 && j < currentField.Rows-1)//3
+
+                        if (i >= 1 && j < currentField.Rows - 1)//3
                         {
                             if (currentField.Filed[i - 1][j + 1] == true) { counter++; }
                         }
+
                         if (j >= 1)//4
                         {
                             if (currentField.Filed[i][j - 1] == true) { counter++; }
                         }
+
                         if (j < currentField.Rows - 1)//5
                         {
                             if (currentField.Filed[i][j + 1] == true) { counter++; }
                         }
+
                         if (i < currentField.Cols - 1 && j >= 1)//6
                         {
                             if (currentField.Filed[i + 1][j - 1] == true) { counter++; }
                         }
+
                         if (i < currentField.Cols - 1)//7
                         {
                             if (currentField.Filed[i + 1][j] == true) { counter++; }
                         }
+
                         if (i < currentField.Cols - 1 && j < currentField.Rows - 1)//8
                         {
                             if (currentField.Filed[i + 1][j + 1] == true) { counter++; }
@@ -61,15 +66,11 @@ namespace Lifegame
                             resultField.Filed[i][j] = false;
                         }
 
-                        if (counter > 3)
-                        {
-                            resultField.Filed[i][j] = false;
-                        }
-
                         if (counter == 3)
                         {
                             resultField.Filed[i][j] = true;
                         }
+
                         if (counter == 2 && cur == true)
                         {
                             resultField.Filed[i][j] = true;
